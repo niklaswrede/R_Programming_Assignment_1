7c450443c931680c3e904ea0bdb7d3e78bbb1501
Niklas Wrede <niklas.wrede@charite.de>
2020-04-07 18:05:57 +0200

    corr geschrieben

--- pollutantmean.R
+++ pollutantmean.R
@@ -20,3 +20,19 @@ complete <- function(directory, id = 1:332) {
 data.frame(id, nobs)
   
 }
+
+
+corr <- function(directory, threshold = 0) {
+  filelist <- list.files(path = directory, pattern = ".csv", full.names = TRUE)
+  correlations <- c()
+  
+  for (i in 1:length(filelist)) {
+    data <- read.csv(filelist[i])
+    no_na <- na.exclude(data)
+    if(nrow(no_na) >= threshold) {
+      correlations <- c(correlations, cor(no_na$nitrate, no_na$sulfate))
+    }
+  }
+  correlations
+  
+}
